66eefaa47cd5efb97186622e10e26ceea5cc7851
linkuwu <89706120+linkuwu@users.noreply.github.com>
2021-09-24 20:04:13 -0500

    Add files via upload

---
+++ Practica1.py
@@ -0,0 +1,12 @@
+from flask import Flask # Import the class `Flask` from the `flask` module, written by someone else.
+
+app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
+
+
+@app.route("/")
+def index():
+    return f"poner 3 valores en el url /a/b/c"
+
+@app.route("/<string:a>/<string:b>/<string:c>")
+def sin_nombre(a,b,c):
+    return f"La fecha es: {a}/{b}/{c} "
